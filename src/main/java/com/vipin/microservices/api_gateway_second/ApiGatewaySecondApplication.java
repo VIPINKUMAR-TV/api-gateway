af309667ced68937f5f7a80dda9426d4d7c7bfa8
Vipinkumar <tvkumarvipin@gmail.com>
2025-04-05 19:40:39 +0530

    api gateway step1

---
+++ src/main/java/com/vipin/microservices/api_gateway_second/ApiGatewaySecondApplication.java
@@ -0,0 +1,13 @@
+package com.vipin.microservices.api_gateway_second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewaySecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewaySecondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vipin/microservices/api_gateway_second/ApiGatewaySecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vipin.microservices.api_gateway_second;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewaySecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
